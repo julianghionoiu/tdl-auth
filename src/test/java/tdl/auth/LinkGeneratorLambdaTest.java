dd5868627936e7d33363d910598725850baaf489
Petra Barus <petra.barus@gmail.com>
2017-06-23 02:41:32 +0700

    Add error handling. (#8)

---
+++ src/test/java/tdl/auth/LinkGeneratorLambdaTest.java
@@ -0,0 +1,52 @@
+package tdl.auth;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import ro.ghionoiu.kmsjwt.key.KeyOperationException;
+
+public class LinkGeneratorLambdaTest {
+    
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void getLambdaExceptionTypeLabel() {
+        Exception ex1 = new IOException("Message");
+        String label1 = LinkGeneratorLambdaHandler.getLambdaExceptionTypeLabel(ex1);
+        assertEquals(label1, "UnknownException");
+        
+        Exception ex2 = new RuntimeException("Message");
+        String label2 = LinkGeneratorLambdaHandler.getLambdaExceptionTypeLabel(ex2);
+        assertEquals(label2, "UnknownException");
+        
+        Exception ex3 = new TemplateException("Message", null);
+        String label3 = LinkGeneratorLambdaHandler.getLambdaExceptionTypeLabel(ex3);
+        assertEquals(label3, "Email");
+    }
+    
+    @Test
+    public void handleRequestShouldThrowException() throws TemplateException, KeyOperationException, IOException {
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("Email");
+        LinkGeneratorLambdaHandler handler = mock(LinkGeneratorLambdaHandler.class);
+        doCallRealMethod().when(handler).handleRequest(any(), any());
+        Exception ex = new TemplateException("Message", null);
+        doThrow(ex).when(handler).getUploadPageUrlFromRequest(any(), any());
+        
+        Context context = mock(Context.class);
+        LambdaLogger logger = mock(LambdaLogger.class);
+        doNothing().when(logger).log(anyString());
+        doReturn(logger).when(context).getLogger();
+        
+        handler.handleRequest(mock(Map.class), context);
+    }
+}
