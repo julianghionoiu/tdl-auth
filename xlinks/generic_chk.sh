9c7140e41e19b457ff99e8ab2a45df3f0870860c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-10-15 08:01:43 +0100

    Add link generation scripts

---
+++ xlinks/generic_chk.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! $# -eq 2 ]; then echo "Usage: $0 company username"; exit 1; fi
+COMPANY=$1
+USERNAME=$2
+
+PAYLOAD_FILE=$(mktemp)
+cat << EOF > ${PAYLOAD_FILE}
+{
+  "headerImageName": "mountain.jpg",
+  "mainChallengeTitle": "Insights",
+  "sponsorName": "${COMPANY}",
+  "codingDurationLabel": "can be solved in 2-3 hours",
+  "allowNoVideoOption": true,
+  "username":"${USERNAME}",
+  "validityDays": 21,
+  "warmupChallenges": [ "SUM", "HLO" ],
+  "officialChallenge": "CHK"
+}
+EOF
+
+aws lambda invoke \
+--invocation-type RequestResponse \
+--function-name tdl-generate-intro \
+--region eu-west-2 \
+--log-type Tail \
+--payload "file://${PAYLOAD_FILE}" \
+./build/outputfile.txt
+cat ./build/outputfile.txt
+cat ./build/outputfile.txt | tr -d "\"" | sed  's/https/http/' | pbcopy
---
+++ xlinks/generic_fiz.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! $# -eq 2 ]; then echo "Usage: $0 company username"; exit 1; fi
+COMPANY=$1
+USERNAME=$2
+
+PAYLOAD_FILE=$(mktemp)
+cat << EOF > ${PAYLOAD_FILE}
+{
+  "headerImageName": "mountain.jpg",
+  "mainChallengeTitle": "FizzDeluxe",
+  "sponsorName": "${COMPANY}",
+  "codingDurationLabel": "can be solved in 45 minutes",
+  "allowNoVideoOption": true,
+  "username":"${USERNAME}",
+  "validityDays": 21,
+  "warmupChallenges": [ "SUM", "HLO" ],
+  "officialChallenge": "FIZ"
+}
+EOF
+
+aws lambda invoke \
+--invocation-type RequestResponse \
+--function-name tdl-generate-intro \
+--region eu-west-2 \
+--log-type Tail \
+--payload "file://${PAYLOAD_FILE}" \
+./build/outputfile.txt
+cat ./build/outputfile.txt
+cat ./build/outputfile.txt | tr -d "\"" | sed  's/https/http/' | pbcopy
