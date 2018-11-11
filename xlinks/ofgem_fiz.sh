7c8b448e09e73195ec476f8bf03778685e01ddf9
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-11 22:13:42 +0000

    Update header

---
+++ xlinks/ofgem_fiz.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -eq 0 ]; then echo "Usage: $0 username"; exit 1; fi
+USERNAME=$1
+
+PAYLOAD_FILE=$(mktemp)
+cat << EOF > ${PAYLOAD_FILE}
+{
+  "headerImageName": "ofgem.jpg",
+  "mainChallengeTitle": "FizzDeluxe",
+  "sponsorName": "Ofgem",
+  "codingDurationLabel": "can be solved in 45 minutes",
+  "allowNoVideoOption": true,
+  "username":"${USERNAME}",
+  "validityDays": 14,
+  "warmupChallenges": [ "SUM", "HLO" ],
+  "officialChallenge": "FIZ"
+}
+EOF
+
+aws lambda invoke \
+--invocation-type RequestResponse \
+--function-name tdl-generate-intro \
+--region eu-west-2 \
+--log-type Tail \
+--payload "file://${PAYLOAD_FILE}" \
+./build/outputfile.txt
+cat ./build/outputfile.txt
+cat ./build/outputfile.txt | tr -d "\"" | sed  's/https/http/' | pbcopy
